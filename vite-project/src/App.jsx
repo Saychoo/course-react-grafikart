59c7e9752d11c7583eb302a33eb5bb407d930174
Vincent Rodriguez <vins_380@hotmail.fr>
2024-02-05 18:49:15 +0100

    useState

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 function App() {
-  return 'Bonjour les gens'
+  const [person, setPerson] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+  });
+
+  const [count, setCount] = useState(0);
+
+  const incrementAge = () => {
+    setCount(count + 1);
+    setPerson({ ...person, age: person.age + 1 });
+  };
+
+  return (
+    <>
+      <p>
+        Age de {person.firstName} : {person.age} - incrément : {count}
+      </p>
+      <button onClick={incrementAge}>Gagner une année</button>
+    </>
+  );
 }
 
-export default App
+export default App;
